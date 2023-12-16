88dab12b04a6c4a09d642817b0a1c372248052ee
Lyubov Gavrilova <suslik.lyuba@gmail.com>
2023-12-16 14:46:51 +0300

    Seminar1 Project1 added

---
+++ Seminar1/Project1/Project1/NewViewController.swift
@@ -0,0 +1,19 @@
+//
+//  NewViewController.swift
+//  Project1
+//
+//  Created by User on 15.12.2023.
+//
+
+import UIKit
+
+class NewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+        title = "NewViewController"
+    }
+    
+
+}
