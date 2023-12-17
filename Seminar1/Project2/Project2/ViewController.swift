2d996e290426c412928aa5e20f960de716ada914
Lyubov Gavrilova <suslik.lyuba@gmail.com>
2023-12-17 21:26:45 +0300

    Seminar1 Project2 added

---
+++ Seminar1/Project2/Project2/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by User on 16.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var logoImageView: UIImageView!
+    
+    @IBOutlet weak var autorizationLabel: UILabel!
+    
+    @IBOutlet weak var LoginField: UITextField!
+    
+    @IBOutlet weak var PasswordField: UITextField!
+    
+    @IBOutlet weak var enterButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
